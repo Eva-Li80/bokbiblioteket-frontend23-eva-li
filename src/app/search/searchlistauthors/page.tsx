1357f60b255ce281cf204374d0f73c6351043249
Eva-Li80 <evaliboden@gmail.com>
2024-05-12 12:45:55 +0200

    lists

--- src/app/search/searchlistauthors/page.tsx
+++ src/app/search/searchlistauthors/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import { RootState } from '@/app/globalredux/store';
 import { Author } from '@/app/lib/types';
-import Link from 'next/link';
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import styles from "../search.module.scss"
 import { addToFavoritAuthor, selectAuthor } from '@/app/globalredux/feature/books/authorSlice';
+import List from '@/app/components/List';
+import AuthorDetails from '@/app/components/AuthorDetails';
+import Modal from '@/app/components/Module';
 export default function page() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
     const authors = useSelector((state: RootState) => state.authors.authors);
     const selectedAuthor = useSelector((state: RootState) => state.authors.selectedAuthor);
     const dispatch = useDispatch();
@@ -18,31 +21,36 @@ export default function page() {
     
     const handleAddAuthor = (author: Author) => {
       dispatch(addToFavoritAuthor(author)); 
-      dispatch(selectAuthor(null)); 
       setSearch("")
+      setModalIsOpen(true);
+
+      setTimeout(() => {
+        setModalIsOpen(false);
+      }, 3000);
     };
   
   return (
     <div className={styles.favoritlist}>
-         {selectedAuthor ? (
         <div>
-          <h2>Name: {selectedAuthor.name}</h2>
-          <p>Birthdate: {selectedAuthor.birth_date}</p>
-          <p>Top subject: {selectedAuthor.top_subjects}</p>
-          <p>Top work{selectedAuthor.top_work}</p>
-          <p>Work count: {selectedAuthor.work_count}</p>
+        <Modal isOpen={modalIsOpen} />
+      </div>
+         {selectedAuthor ? (
+        <div className={styles.favoritlistdetails}>
+         <AuthorDetails author={selectedAuthor}/>
           <button onClick={() => handleAddAuthor(selectedAuthor)}>Add author to your authorlist..</button>
         </div>
       ) : (
-        <div className={styles.favoritlist}>
-          {authors.map((author: Author, index: number) => (
-            <div key={index} onClick={() => handleAuthorClick(author)}>
+       <div className={styles.favoritlist}>
+        <h2>Searched authors</h2>
+        <List items={authors} onClick={handleAuthorClick} typeToRender={(author: Author) => (
+          <div>
               <h2 className={styles.favoritlistitem}>
               <img src={author.imageUrl} alt={author.name} style={{width:100, height: 150}} />
                 {author.name}</h2>
               
             </div>
-          ))}
+        )}/>
+          
         </div>
       )}
       
